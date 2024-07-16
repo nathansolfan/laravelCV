a5221165f3a41b4daca91986596670929058323a
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-07-16 19:08:55 +0100

    feat: Add homepage route and view
    The code changes include adding a new route and corresponding view for the homepage. This allows users to access the home page of the Laravel application.
    
    Based on the recent user commits and repository commits, it seems that the focus of this commit is on adding functionality related to the homepage.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+    <h1>Welcome to the Home Page</h1>
+    <p>This is a simple home page for your Laravel application.</p>
+    <nav>
+        <ul>
+            <li><a href="{{url('/about')}}">About me</a></li>
+            <li><a href="{{url('/contact')}}">Contact me</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 // STANDARD LAYOUT
 // Route::get('/', function () {
@@ -8,3 +9,6 @@ use Illuminate\Support\Facades\Route;
 
 // route listens to GET - request the root URL / and 'index' from HomeController
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/contact', [PageController::class, 'contact']);
