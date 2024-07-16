8883de7e3d4118da983fd6bfb96222f0c3dde7a3
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-07-16 12:25:27 +0100

    Created route to homepage

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+// STANDARD LAYOUT
+// Route::get('/', function () {
+//     return view('welcome');})
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// route listens to GET - request the root URL / and 'index' from HomeController
+Route::get('/', [HomeController::class, 'index']);
